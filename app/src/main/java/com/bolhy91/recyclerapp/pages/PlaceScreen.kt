d56c64d3ce39e233653095220ef78fa8cb480da3
bolhy91 <bolhy91@gmail.com>
2022-11-12 01:33:50 -0500

    calculo de los puntos

--- app/src/main/java/com/bolhy91/recyclerapp/pages/PlaceScreen.kt
+++ app/src/main/java/com/bolhy91/recyclerapp/pages/PlaceScreen.kt
@@ -1,5 +1,7 @@
 package com.bolhy91.recyclerapp.pages
 
+import android.location.Location
+import android.util.Log
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
@@ -13,24 +15,37 @@ import com.google.maps.android.compose.*
 fun PlaceScreen(
     viewModel: PlaceViewModel = hiltViewModel()
 ) {
-    val city = LatLng(9.0817275, -79.5932217)
+    val city = LatLng(9.0182406, -79.5045156)
+
+    val myLocation = Location("Mi Casa")
+    myLocation.latitude = 8.9984378
+    myLocation.longitude = -79.5227452
+
     val cameraPositionState = rememberCameraPositionState {
-        position = CameraPosition.fromLatLngZoom(city, 10f)
+        position = CameraPosition.fromLatLngZoom(city, 12f)
     }
     val state = viewModel.state.value
     GoogleMap(
-        modifier = Modifier.fillMaxSize(), cameraPositionState = cameraPositionState,
+        modifier = Modifier.fillMaxSize(),
+        cameraPositionState = cameraPositionState,
         properties = MapProperties(mapType = MapType.NORMAL)
     ) {
         if (state.places.isNotEmpty()) {
             state.places.forEach { place ->
-                Marker(
-                    state = MarkerState(
-                        position = LatLng(
-                            place.latitude.toDouble(), place.longitude.toDouble()
-                        )
-                    ), title = place.name, snippet = place.description
-                )
+                val valor = Location("")
+                valor.latitude = place.latitude.toDouble()
+                valor.longitude = place.longitude.toDouble()
+                val distance = (myLocation.distanceTo(valor)/1000)
+                Log.i("DISTANCE: ${place.name}", (distance/1000).toString())
+                if (distance <= 5) {
+                    Marker(
+                        state = MarkerState(
+                            position = LatLng(
+                                place.latitude.toDouble(), place.longitude.toDouble()
+                            )
+                        ), title = place.name, snippet = place.description
+                    )
+                }
             }
         }
     }
